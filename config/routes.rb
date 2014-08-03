4bfce5ba02092d6bad1540100fbfb3b8ed842e19
Piotr Brzezinski <piotr.brzezinski@netguru.pl>
2014-08-03 14:39:50 +0200

    login styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Nuller::Application.routes.draw do
   get "/", :to => "main_page#index", as: 'user_root'
   devise_for :user do
     get "/login", :to => "devise/sessions#new" # Add a custom sign in route for user sign in
-    get "/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
+    get "/users/sign_out", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
     get "/register", :to => "devise/registrations#new" # Add a Custom Route for Registrations
   end
 end
